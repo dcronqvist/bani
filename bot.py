3f8caec162f7ffa4a1fa8beafcb882f305db73b5
Daniel Cronqvist <dancro99@gmail.com>
2021-06-20 01:47:48 +0200

    added a bit of announcing to i know when it's updated

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import json
+from typing import Text
 import discord
+from discord.abc import GuildChannel
+from discord.channel import TextChannel
 from discord.message import Message
 import config as conf
 import logging
 import commands.pihole as pi
+import discord.ext.commands as commands
+import random as rand
 
 # create logger
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
@@ -14,9 +19,27 @@ cmds = {
     "pihole": pi.get_pihole_status_json
 }
 
+def get_random_emoji():
+    return rand.choice([
+        ":duck:",
+        ":dodo:",
+        ":bird:",
+        ":baby_chick:"
+    ])
+
+version = "v0.1-alpha"
 class MyClient(discord.Client):
+
     async def on_ready(self):
         logging.info(f"Successfully logged in as {self.user}")
+
+        for guild in self.guilds:
+            for channel in guild.channels:
+                if type(channel) is TextChannel:
+                    if channel.permissions_for(channel.guild.me).send_messages == True:
+                        await channel.send(f"🤖 `{command}` is ready to go! running version `{version}` {get_random_emoji()}")
+                        logging.info(f"Announced in {channel} in {guild}")
+            
 
     async def on_message(self, message : Message):
         # don't respond to ourselves
@@ -37,8 +60,6 @@ class MyClient(discord.Client):
             await run(message)
 
         #----------------------------
-        if "die" in message.content:
-            client.close()
 
 client = MyClient()
 logging.info("Running bot...")
